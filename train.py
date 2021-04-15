d8d19b84a71846ae2f819aeee28f386ca0956be5
Daniele Canavese <daniele.canavese@polito.it>
2021-04-15 15:47:01 +0200

    Added the --seed option

--- train.py
+++ train.py
@@ -1,12 +1,15 @@
 """
-Trains a new IDS.
+Trains a new traffic analyzer.
 """
 
 from argparse import ArgumentParser
 from configparser import ConfigParser
+from random import seed as random_seed
 
 from blessed import Terminal
 from joblib import dump
+from numpy.random import seed as numpy_seed
+from torch import manual_seed as torch_seed
 
 from netgen import NetGen
 
@@ -14,6 +17,7 @@ from netgen import NetGen
 parser = ArgumentParser(description="Trains a new traffic analyzer.")
 parser.add_argument("--quiet", action="store_true", help="disables the logs")
 parser.add_argument("--test", default="test", help="the test report folder")
+parser.add_argument("--seed", type=int, default=42, help="the seed for the pseudo-random number generators")
 parser.add_argument("--config", default="netgen.conf", help="the name of the configuration file")
 parser.add_argument("--model", default="model.joblib", help="the name of the generated model file")
 parser.add_argument("data", help="the name of the data file")
@@ -24,6 +28,10 @@ configuration = ConfigParser()
 configuration.read(args.config)
 
 terminal = Terminal()
+
+random_seed(args.seed)
+numpy_seed(args.seed)
+torch_seed(args.seed)
 
 # Trains! A lot of trains!
 netgen = NetGen(configuration, not args.quiet)
